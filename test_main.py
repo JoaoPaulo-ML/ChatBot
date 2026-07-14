c281c80934c57ddf06a7b1ecad75f9734190c420
Joao Paulo <joaopaulomarques654@gmail.com>
2026-07-13 20:36:14 -0400

    test(db): cria testes para retorno dos estoques

---
+++ test_main.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+from main import app, buscar_estoque_no_banco
+
+client = TestClient(app)
+
+def test_buscar_estoque_no_banco():
+    """
+    TESTE 1: Teste Unitário.
+    Testa isoladamente se a função de buscar no SQLite está funcionando
+    e retornando os dados no formato correto.
+    """
+    estoque = buscar_estoque_no_banco()
+   
+    assert isinstance(estoque, list)
+    
+    assert len(estoque) > 0
+    
+    primeiro_peixe = estoque[0]
+    
+    assert len(primeiro_peixe) == 2
+    
+    assert isinstance(primeiro_peixe[0], str)
+    assert isinstance(primeiro_peixe[1], int)
